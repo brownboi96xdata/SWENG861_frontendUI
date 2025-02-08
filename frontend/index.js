89db321adad5981e3223cba7d5b368b99e1aee1e
Barath Alliyalamangalam <kma6596@psu.edu>
2025-02-08 13:57:14 -0800

    Installed react vite files

--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-// Create this file inside frontend/
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app.js";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
