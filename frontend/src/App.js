a8387d9d4a13e9d6ab6b6d855a506b3df1507633
Barath Alliyalamangalam <kma6596@psu.edu>
2025-02-08 21:03:41 -0800

    Added react files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import GameList from './GameList';
+
+function App() {
+  return (
+    <div>
+      <GameList />
+    </div>
+  );
+}
+
+export default App;
