8facff9caf410242b44101673be7030403790336
Barath Alliyalamangalam <kma6596@psu.edu>
2025-02-12 13:45:41 -0800

    Frontend edited .env and App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = "https://www.cheapshark.com/api/1.0/games";
+// Use the environment variable for the API URL
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
     const [games, setGames] = useState([]);
